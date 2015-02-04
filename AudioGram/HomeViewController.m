2bc41a7dbb7e27760a2bbd573636986f35890b3b
Mary Jenel <mmyers7@hawaii.edu>
2015-02-03 19:28:13 -0800

    added Login Parse

--- AudioGram/HomeViewController.m
+++ AudioGram/HomeViewController.m
@@ -9,8 +9,9 @@
 #import "HomeViewController.h"
 #import "ImageCollectionViewCell.h"
 #import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
 
-@interface HomeViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
+@interface HomeViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, PFSignUpViewControllerDelegate,PFLogInViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UICollectionView *homeCollectionView;
 @property NSArray *photoArray;
 @end
@@ -29,8 +30,64 @@
 
     }];
 
-
     
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if (![PFUser currentUser])
+    {
+        PFLogInViewController *loginViewController = [[PFLogInViewController alloc]init];
+        [loginViewController setDelegate:self];
+
+        PFSignUpViewController *signUpViewController = [[PFSignUpViewController alloc]init];
+        [signUpViewController setDelegate:self];
+
+        [loginViewController setSignUpController:signUpViewController];
+
+        [self presentViewController:loginViewController animated:YES completion:nil];
+
+    }
+
+}
+
+-(BOOL)logInViewController:(PFLogInViewController *)logInController shouldBeginLogInWithUsername:(NSString *)username password:(NSString *)password
+{
+    if (username && password && username.length != 0 && password.length != 0)
+    {
+        return YES;
+    }
+    [[[UIAlertView alloc]initWithTitle:@"Missing Information!" message:@"Make sure you fill out all the information, please!" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles: nil]show];
+    return NO;
+}
+
+-(void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+-(BOOL)signUpViewController:(PFSignUpViewController *)signUpController shouldBeginSignUp:(NSDictionary *)info
+{
+    BOOL informationComplete = YES;
+    for (id key in info)
+    {
+        NSString *field = [info objectForKey:key];
+        if (!field || field.length == 0) {
+            informationComplete = NO;
+            break;
+        }
+    }
+    if (!informationComplete)
+    {
+        [[[UIAlertView alloc]initWithTitle:@"Missing Information" message:@"Make sure you fill out all of the information!" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles: nil]show];
+    }
+    return informationComplete;
 }
 
 -(ImageCollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
